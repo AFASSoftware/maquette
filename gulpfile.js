f93444d08b6f8e5cfcced3ed754bab61953fe097
Johan Gorter <johan@johangorter.com>
2014-12-19 22:30:34 +0100

    moving stuff around

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp=require("gulp");
+var uglify=require("gulp-uglify");
+var rename = require('gulp-rename');
+var del = require('del');
+
+gulp.task("compress",  function(){
+  gulp.src("src/*.js")
+    .pipe(uglify())
+	.pipe(rename({suffix: '.min'}))
+	.pipe(gulp.dest("dist"))
+});
+
+gulp.task('clean', function(cb) {
+  del(['dist'], cb);
+});
+
+gulp.task("default", ["compress"]);

b71ee40f91b7f39d384dd91a45f3b91eae4b7f4f
Johan Gorter <johan@johangorter.com>
2015-02-17 22:50:50 +0100

    more levels

---
+++ tutorial/assets/saucer.js
@@ -0,0 +1,27 @@
+﻿var h = maquette.h;
+
+var rotation = 45;
+
+function handleRotationInput(evt) {
+  rotation = evt.target.value;
+};
+
+function renderMaquette() {
+  return h("body", [
+    h("div.landscape", [
+      h("div.remote", {}, [
+        h("div.function", [
+          "rotation: ",
+          h("input", { type: "text", value: rotation, oninput: handleRotationInput }),
+          "degrees"
+        ])
+      ]),
+      h("div.saucer", {style:"transform:rotate("+rotation+"deg)"}, ["Flying saucer"])
+    ])
+  ]);
+}
+
+// Initializes the projector 
+document.addEventListener('DOMContentLoaded', function () {
+  maquette.createProjector(document.body, renderMaquette);
+});

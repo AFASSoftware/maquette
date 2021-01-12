2832257c0d2e46a6e0c34c8ba9f8aeb049f16708
Johan Gorter <johan-gorter@users.noreply.github.com>
2021-01-12 09:26:53 +0100

    fixes #164 inline styles specified using camelCase property names

--- src/dom.ts
+++ src/dom.ts
@@ -9,7 +9,13 @@ const DEFAULT_PROJECTION_OPTIONS: ProjectionOptions = {
   performanceLogger: () => undefined,
   eventHandlerInterceptor: undefined,
   styleApplyer: (domNode: HTMLElement, styleName: string, value: string) => {
-    domNode.style.setProperty(styleName, value);
+    if (styleName.charAt(0) === '-') {
+      // CSS variables must be set using setProperty
+      domNode.style.setProperty(styleName, value);
+    } else {
+      // properties like 'backgroundColor' must be set as a js-property
+      (domNode.style as any)[styleName] = value;
+    }
   }
 };
 
